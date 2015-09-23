e5c43864dd676842b563727aaa71694fdade3fad
Josh Terrell <joshterrell805@gmail.com>
2015-09-23 13:01:11 -0700

    promise latest history given a channelId

---
+++ pYoutubeLatestHistoryFromChannelId.js
@@ -0,0 +1,25 @@
+module.exports = pYoutubeLatestHistoryFromChannelId;
+
+var pRequestYoutube = require('./pRequestYoutube'),
+    _ = require('lodash');
+
+// this is the hard top-limit from youtube
+// https://developers.google.com/youtube/v3/docs/activities/list
+var maxResults = 50;
+
+function pYoutubeLatestHistoryFromChannelId(channelId) {
+  return pRequestYoutube('activities', {part: 'snippet', channelId: channelId,
+      maxResults: maxResults})
+  .then(function(res) {
+    return _.pluck(res.items, 'snippet');
+  });
+}
+
+if (!module.parent) {
+  var channelId = process.argv[2] || 'UC6nSFpj9HTCZ5t-N3Rm3-HA';
+  pYoutubeLatestHistoryFromChannelId(channelId)
+  .then(function(res) {
+    console.log(util.inspect(res, false, null));
+  })
+  .done();
+}
