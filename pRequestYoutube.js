1a7ba5c423591bd8d719c6ce9bc518f933823678
Josh Terrell <joshterrell805@gmail.com>
2015-09-23 13:16:46 -0700

    pRequestYoutube: Optionally request all pages and combine results in items

--- pRequestYoutube.js
+++ pRequestYoutube.js
@@ -6,11 +6,23 @@ var request = require('request-promise'),
 
 var apiKey = process.env.GOOGLE_API_KEY;
 
-function pRequestYoutube(endpoint, options) {
+function pRequestYoutube(endpoint, options, allPages) {
   _.defaults(options, options, {key: apiKey});
   return request('https://content.googleapis.com/youtube/v3/' + endpoint + '?' +
       querystring.stringify(options))
-  .then(JSON.parse);
+  .then(JSON.parse)
+  .then(function(res) {
+    if (allPages && res.nextPageToken) {
+      options = _.defaults({}, {pageToken: res.nextPageToken}, options);
+      return pRequestYoutube(endpoint, options, true)
+      .then(function(res2) {
+        res.items = res.items.concat(res2.items);
+        return res;
+      });
+    } else {
+      return res;
+    }
+  });
 }
 
 if (!module.parent) {
@@ -18,7 +30,8 @@ if (!module.parent) {
   var endpoint = process.argv[2] || 'channels';
   var options = process.argv[3] ? JSON.parse(process.argv[3]) :
       {part: 'id', forUsername: 'vsauce'};
-  pRequestYoutube(endpoint, options)
+  var allPages = process.argv[4] === 'true';
+  pRequestYoutube(endpoint, options, allPages)
   .then(function(res) {
     console.log(util.inspect(res, false, null));
   })
