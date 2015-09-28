d0e461ba53558bc57af15993792f2b24de07a50b
Josh Terrell <joshterrell805@gmail.com>
2015-09-28 09:46:47 -0700

    add svm and k-nearest-neighbors classifiers

--- train/train.py
+++ train/train.py
@@ -1,12 +1,25 @@
 from sklearn import linear_model
+from sklearn import svm
+from sklearn import neighbors
 import dataset
 
 def train():
-    logreg = linear_model.LogisticRegression(C=1e2)
+    mod_logreg = linear_model.LogisticRegression(C=1e2)
+    mod_svm = svm.SVC(C=1e2)
+    mod_neigh = neighbors.KNeighborsClassifier(n_neighbors=3)
+
     X, y = dataset.getDataset()
-    logreg.fit(X, y)
-    accuracy = logreg.score(X, y)
-    print(accuracy)
+
+    mod_logreg.fit(X, y)
+    mod_svm.fit(X, y)
+    mod_neigh.fit(X, y)
+
+    accuracy = mod_logreg.score(X, y)
+    print('logistic regression accuracy: ', accuracy)
+    accuracy = mod_svm.score(X, y)
+    print('svm accuracy: ', accuracy)
+    accuracy = mod_neigh.score(X, y)
+    print('nearest neighbor accuracy: ', accuracy)
 
 if __name__ == '__main__':
     train()
