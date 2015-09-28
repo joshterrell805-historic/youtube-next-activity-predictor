885c6ab5216403ec1db6d0176329adde6b3b8f99
Josh Terrell <joshterrell805@gmail.com>
2015-09-28 13:32:00 -0700

    Evaluate examples on test set rather than train set

--- train/train.py
+++ train/train.py
@@ -2,6 +2,8 @@ from sklearn import linear_model
 from sklearn import svm
 from sklearn import neighbors
 import dataset
+import numpy
+import math
 
 def train():
     mod_logreg = linear_model.LogisticRegression(C=1e2)
@@ -10,10 +12,18 @@ def train():
 
     X, y = dataset.getDataset()
 
+    examples = list(zip(X, y))
+    numpy.random.shuffle(examples)
+    testExampleCount = math.floor(0.80 * len(examples))
+    trainExamples = examples[:testExampleCount]
+    testExamples = examples[testExampleCount:]
+
+    X, y = zip(*trainExamples)
     mod_logreg.fit(X, y)
     mod_svm.fit(X, y)
     mod_neigh.fit(X, y)
 
+    X, y = zip(*testExamples)
     accuracy = mod_logreg.score(X, y)
     print('logistic regression accuracy: ', accuracy)
     accuracy = mod_svm.score(X, y)
